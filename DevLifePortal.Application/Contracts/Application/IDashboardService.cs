6fe92ff63f4b30cc42eb78749a803af11c7813c0
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 18:29:18 +0400

    add dashboard service interface

---
+++ DevLifePortal.Application/Contracts/Application/IDashboardService.cs
@@ -0,0 +1,9 @@
+﻿using DevLifePortal.Application.DTOs;
+
+namespace DevLifePortal.Application.Contracts.Application
+{
+    public interface IDashboardService
+    {
+        DashboardDTO GenerateDashboard(string username);
+    }
+}
