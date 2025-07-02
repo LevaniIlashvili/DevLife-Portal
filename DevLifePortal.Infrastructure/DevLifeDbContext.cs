5c07c3ef3eb8bb1e3365dd1b401a3949fcdb6a70
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:20:46 +0400

    add dbcontext for postgresql

---
+++ DevLifePortal.Infrastructure/DevLifeDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DevLifePortal.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevLifePortal.Infrastructure
+{
+    public class DevLifeDbContext : DbContext  
+    {
+        public DevLifeDbContext(DbContextOptions<DevLifeDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
