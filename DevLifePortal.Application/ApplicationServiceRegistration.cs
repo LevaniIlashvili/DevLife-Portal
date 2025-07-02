ec5972475523a9e8fd58215cfc740bab02a4b5ca
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:26:49 +0400

    add class to register application services

---
+++ DevLifePortal.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using DevLifePortal.Application.Contracts.Application;
+using DevLifePortal.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevLifePortal.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
