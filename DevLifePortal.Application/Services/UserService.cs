05b4c92fdb309e9b12708015caef6886b8aa68ab
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:26:24 +0400

    add user service

---
+++ DevLifePortal.Application/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using DevLifePortal.Application.Contracts.Application;
+using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User?> GetUserByUsernameAsync(string username)
+        {
+            return await _userRepository.GetByUsernameAsync(username);
+        }
+
+        public async Task<User> RegisterUser(User newUser)
+        {
+            var existingUser = await _userRepository.GetByUsernameAsync(newUser.Username);
+
+            if (existingUser != null)
+            {
+                throw new Exceptions.BadRequestException("User with this username already exists");
+            }
+
+            return await _userRepository.AddAsync(newUser);
+        }
+    }
+}
