218ddf174112051c2c3b518c943f76e90a8edb0a
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 18:29:31 +0400

    create dashboard dto

---
+++ DevLifePortal.Application/DTOs/DashboardDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class DashboardDTO
+    {
+        public string DailyHoroscope { get; set; }
+        public string DailyCodeAdvice { get; set; }
+        public string LuckyTechnologyOfTheDay { get; set; } 
+    }
+}
