6e7abfcb0f3fb45bec63fa740caf6fe180f10bea
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:26:10 +0400

    add interface for user service

---
+++ DevLifePortal.Application/Contracts/Application/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Contracts.Application
+{
+    public interface IUserService
+    {
+        Task<User?> GetUserByUsernameAsync(string username);
+        Task<User> RegisterUser(User user);
+    }
+}
