18eb3efa4101e44db2fe8b7085ce6242dceb51b8
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:22:34 +0400

    add user repository

---
+++ DevLifePortal.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevLifePortal.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DevLifeDbContext _dbContext;
+
+        public UserRepository(DevLifeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+    }
+}
