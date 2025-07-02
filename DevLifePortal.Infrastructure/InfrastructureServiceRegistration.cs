b01e171eb7756e78dd5ac5ef27cc5d5a0e066e5b
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:23:52 +0400

    add class for infrastructure service registration

---
+++ DevLifePortal.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevLifePortal.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DevLifeDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("PostgreSqlConnection")));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
