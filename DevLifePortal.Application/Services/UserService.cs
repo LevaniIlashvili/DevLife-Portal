62534ec70ae105f0641b9c5dbf69a027ac17fb2b
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 17:22:09 +0400

    calculate and add zodiac sign to user

--- DevLifePortal.Application/Services/UserService.cs
+++ DevLifePortal.Application/Services/UserService.cs
@@ -27,7 +27,33 @@ namespace DevLifePortal.Application.Services
                 throw new Exceptions.BadRequestException("User with this username already exists");
             }
 
+            newUser.ZodiacSign = GetZodiacSign(newUser.DateOfBirth);
+
             return await _userRepository.AddAsync(newUser);
+        }
+
+        private static string GetZodiacSign(DateOnly date)
+        {
+            var day = date.Day;
+            var month = date.Month;
+
+            return (month, day) switch
+            {
+                (1, <= 19) => "Capricorn",
+                (1, >= 20) or (2, <= 18) => "Aquarius",
+                (2, >= 19) or (3, <= 20) => "Pisces",
+                (3, >= 21) or (4, <= 19) => "Aries",
+                (4, >= 20) or (5, <= 20) => "Taurus",
+                (5, >= 21) or (6, <= 20) => "Gemini",
+                (6, >= 21) or (7, <= 22) => "Cancer",
+                (7, >= 23) or (8, <= 22) => "Leo",
+                (8, >= 23) or (9, <= 22) => "Virgo",
+                (9, >= 23) or (10, <= 22) => "Libra",
+                (10, >= 23) or (11, <= 21) => "Scorpio",
+                (11, >= 22) or (12, <= 21) => "Sagittarius",
+                (12, >= 22) or (1, <= 19) => "Capricorn",
+                _ => "Unknown"
+            };
         }
     }
 }
