f3c0ba0febffb3722fcbe54497bdb933c010fcc7
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 18:30:25 +0400

    create dashboard endpoints class

---
+++ DevLifePortal.Api/Endpoints/DashboardEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using DevLifePortal.Application.Contracts.Application;
+
+namespace DevLifePortal.Api.Endpoints
+{
+    public static class DashboardEndpoints
+    {
+        public static void MapDashboardEndpoints(this IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapGet("/dashboard", async (HttpContext httpContext, IUserService userService, IDashboardService dashboardService) =>
+            {
+                var username = httpContext.User.Identity?.Name;
+                if (string.IsNullOrEmpty(username))
+                {
+                    return Results.Unauthorized();
+                }
+
+                var dashboardData = dashboardService.GenerateDashboard(username);
+                return Results.Ok(dashboardData);
+            })
+            .RequireAuthorization()
+            .WithTags("Dashboard");
+        }
+    }
+}
