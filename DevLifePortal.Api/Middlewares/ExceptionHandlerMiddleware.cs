0b58523ff2021f696633ecafb9b6a77861dfb96b
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:27:20 +0400

    create custom exception handler middleware

---
+++ DevLifePortal.Api/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using DevLifePortal.Application.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace DevLifePortal.Api.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await ConvertException(context, ex);
+            }
+        }
+
+        private Task ConvertException(HttpContext context, Exception exception)
+        {
+            HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError;
+
+            context.Response.ContentType = "application/json";
+
+            var result = string.Empty;
+
+            switch (exception)
+            {
+                case BadRequestException:
+                    httpStatusCode = HttpStatusCode.BadRequest;
+                    break;
+
+                default:
+                    _logger.LogError(exception, "An unexpected error occurred.");
+                    result = JsonSerializer.Serialize(new { error = "An unexpected error occurred. Please try again later." });
+                    break;
+            }
+
+            context.Response.StatusCode = (int)httpStatusCode;
+
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
