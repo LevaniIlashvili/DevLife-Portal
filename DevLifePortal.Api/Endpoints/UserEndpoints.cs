908a88ba10f7e71af59ec3549e17a5bda01dffb0
Levani Ilashvili <levanilashvili2005@gmail.com>
2025-07-02 16:27:55 +0400

    create user endpoints

---
+++ DevLifePortal.Api/Endpoints/UserEndpoints.cs
@@ -0,0 +1,62 @@
+﻿using DevLifePortal.Application.Contracts.Application;
+using DevLifePortal.Domain.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace DevLifePortal.Api.Endpoints
+{
+    public static class UserEndpoints
+    {
+        public static IEndpointRouteBuilder MapUserEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("/register", async (IUserService userService, User user) =>
+            {
+                var createdUser = await userService.RegisterUser(user);
+                return Results.Ok(createdUser);
+            });
+
+            app.MapPost("/login", async (HttpContext context, IUserService userService, [FromBody] string username) =>
+            {
+                var user = await userService.GetUserByUsernameAsync(username);
+                if (user == null)
+                {
+                    return Results.Unauthorized();
+                }
+
+                context.Session.SetString("UserId", user.Id.ToString());
+                context.Session.SetString("Username", user.Username);
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.Username)
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                var principal = new ClaimsPrincipal(identity);
+
+                await context.SignInAsync("MyCookieAuth", principal);
+
+                return Results.Ok();
+            });
+
+            app.MapPost("/logout", async (HttpContext context) =>
+            {
+                context.Session.Clear();
+                await context.SignOutAsync("MyCookieAuth");
+                return Results.Ok();
+            });
+
+            app.MapGet("/me", async (HttpContext context, IUserService userService) =>
+            {
+                var username = context.Session.GetString("Username");
+                var user = await userService.GetUserByUsernameAsync(username!);
+
+                return Results.Ok(user);
+            }).RequireAuthorization();
+
+            return app;
+        }
+    }
+}
