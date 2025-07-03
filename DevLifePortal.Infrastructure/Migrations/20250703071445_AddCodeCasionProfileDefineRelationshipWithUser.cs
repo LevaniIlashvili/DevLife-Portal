5b43d628180183c71ca4518fe5d8a62f8c944cff
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 14:58:15 +0400

    add migration for code casino profile

---
+++ DevLifePortal.Infrastructure/Migrations/20250703071445_AddCodeCasionProfileDefineRelationshipWithUser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DevLifePortal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCodeCasionProfileDefineRelationshipWithUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CodeCasinoProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    Points = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CodeCasinoProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CodeCasinoProfiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CodeCasinoProfiles_UserId",
+                table: "CodeCasinoProfiles",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CodeCasinoProfiles");
+        }
+    }
+}
