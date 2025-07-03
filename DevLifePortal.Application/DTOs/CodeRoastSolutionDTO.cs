bc52263a71578a50cd7dfe34b60188d2a6e6336d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 16:14:53 +0400

    add coderoast solution dto

---
+++ DevLifePortal.Application/DTOs/CodeRoastSolutionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class CodeRoastSolutionDTO
+    {
+        public string ProblemName { get; set; }
+        public string Solution { get; set; }
+    }
+}
