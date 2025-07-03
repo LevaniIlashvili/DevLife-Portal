3d545828927d42651c6957df3d9e04e8f8eb498d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 15:08:25 +0400

    create code casino challenge answer dto

---
+++ DevLifePortal.Application/DTOs/CodeCasinoAnswerChallengeDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class CodeCasinoAnswerChallengeDTO
+    {
+        public bool ChoseCorrect {  get; set; }
+        public int PointsWagered { get; set; }
+    }
+}
