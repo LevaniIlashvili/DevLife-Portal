23a2f161fa3fa8a7d57417e863611206bd92f129
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 15:05:17 +0400

    add code casino challenge entity

---
+++ DevLifePortal.Domain/Entities/CodeCasinoChallenge.cs
@@ -0,0 +1,9 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class CodeCasinoChallenge
+    {
+        public string CorrectCode { get; set; } = string.Empty;
+        public string IncorrectCode { get; set; } = string.Empty;
+        public string TechStack { get; set; } = string.Empty;
+    }
+}
