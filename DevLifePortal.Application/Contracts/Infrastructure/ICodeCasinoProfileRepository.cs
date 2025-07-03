d15cf5f7aabe4ece18bdff87cc9774432edeaee3
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 14:59:39 +0400

    add interface for codecasinoprofile repository

---
+++ DevLifePortal.Application/Contracts/Infrastructure/ICodeCasinoProfileRepository.cs
@@ -0,0 +1,11 @@
+﻿using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Contracts.Infrastructure
+{
+    public interface ICodeCasinoProfileRepository
+    {
+        Task CreateProfile(int userId);
+        Task<CodeCasinoProfile> GetProfile(int userId);
+        Task UpdateProfile(CodeCasinoProfile profile);
+    }
+}
