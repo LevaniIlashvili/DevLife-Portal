541f4688830f7ffc7d5a4acc4aff100179c11819
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 14:57:35 +0400

    add code casino profile

---
+++ DevLifePortal.Domain/Entities/CodeCasinoProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class CodeCasinoProfile
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int Points { get; set; }
+
+        public User User { get; set; }
+    }
+}
