de53d323c79101e0e864e3bb098a46a7770a2006
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 16:15:23 +0400

    add endpoints for code roast

---
+++ DevLifePortal.Api/Endpoints/CodeRoastEndpoints.cs
@@ -0,0 +1,26 @@
+﻿using DevLifePortal.Application.Contracts.Application;
+using DevLifePortal.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevLifePortal.Api.Endpoints
+{
+    public static class CodeRoastEndpoints
+    {
+        public static void MapCodeRoastEndpoints(this IEndpointRouteBuilder endpoints)
+        {
+            var codeRoastGroup = endpoints.MapGroup("/coderoast");
+
+            codeRoastGroup.MapGet("/problem", async (ICodeRoastService codeRoastService, [FromQuery] string difficulty = "easy") =>
+            {
+                var problem = await codeRoastService.GetProblem(difficulty);
+                return problem;
+            });
+
+            codeRoastGroup.MapPost("/problem", async (ICodeRoastService codeRoastService, [FromBody] CodeRoastSolutionDTO solutionDTO) =>
+            {
+                var roast = await codeRoastService.RoastCode(solutionDTO.ProblemName, solutionDTO.Solution);
+                return roast;
+            });
+        }
+    }
+}
