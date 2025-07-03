3272af7db40fa476189101936a06e3c2d0977a32
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 14:59:10 +0400

    add code casino profile repository

---
+++ DevLifePortal.Infrastructure/Repositories/CodeCasinoProfileRepository.cs
@@ -0,0 +1,36 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevLifePortal.Infrastructure.Repositories
+{
+    public class CodeCasinoProfileRepository : ICodeCasinoProfileRepository
+    {
+        private readonly DevLifeDbContext _dbContext;
+
+        public CodeCasinoProfileRepository(DevLifeDbContext devLifeDbContext)
+        {
+            _dbContext = devLifeDbContext;
+        }
+
+        public async Task CreateProfile(int userId)
+        {
+            var profile = new CodeCasinoProfile { UserId = userId, Points = 100 };
+            await _dbContext.CodeCasinoProfiles.AddAsync(profile);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<CodeCasinoProfile> GetProfile(int userId)
+        {
+            var profile = await _dbContext.CodeCasinoProfiles.FirstAsync(profile => profile.UserId == userId);
+            return profile;
+        }
+
+        public async Task UpdateProfile(CodeCasinoProfile profile)
+        {
+            var profileToUpdate = await _dbContext.CodeCasinoProfiles.FindAsync(profile.Id);
+            profileToUpdate.Points = profile.Points;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
