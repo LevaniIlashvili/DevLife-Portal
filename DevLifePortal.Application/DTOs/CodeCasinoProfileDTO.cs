17c9015f288e7e72a3259cd868d01d0b922c214c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-03 15:07:42 +0400

    add code casino profile dto

---
+++ DevLifePortal.Application/DTOs/CodeCasinoProfileDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class CodeCasinoProfileDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int Points { get; set; }
+    }
+}
