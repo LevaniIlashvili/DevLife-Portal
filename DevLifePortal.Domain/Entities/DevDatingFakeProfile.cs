23d505bdd99aa161de1389ad5584d4540db486a4
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:13:30 +0400

    create devdating fake profile entity

---
+++ DevLifePortal.Domain/Entities/DevDatingFakeProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class DevDatingFakeProfile
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public string Preference { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+        public string TechStack { get; set; } = string.Empty;
+    }
+}
