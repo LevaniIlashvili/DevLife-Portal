8d5d90da86d05900bb400fae31d3894f056e5b3f
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 09:55:54 +0400

    create bug chase profile repository

---
+++ DevLifePortal.Application/Contracts/Infrastructure/IBugChaseProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Contracts.Infrastructure
+{
+    public interface IBugChaseProfileRepository
+    {
+        Task<BugChaseProfile> CreateProfile(int userId);
+        Task<List<BugChaseProfile>> GetTopProfiles();
+        Task UpdateProfile(BugChaseProfile bugChaseProfile);
+        Task<BugChaseProfile> GetProfile(int userId);
+    }
+}
---
+++ DevLifePortal.Infrastructure/Repositories/BugChaseProfileRepository.cs
@@ -0,0 +1,50 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevLifePortal.Infrastructure.Repositories
+{
+    public class BugChaseProfileRepository : IBugChaseProfileRepository
+    {
+        private readonly DevLifeDbContext _dbContext;
+
+        public BugChaseProfileRepository(DevLifeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<BugChaseProfile> GetProfile(int userId)
+        {
+            var profile = await _dbContext.BugChaseProfiles.FirstAsync(p => p.UserId == userId);
+            return profile;
+        }
+
+        public async Task<List<BugChaseProfile>> GetTopProfiles()
+        {
+            var profiles = await _dbContext.BugChaseProfiles.OrderByDescending(p => p.MaxScore).Take(10).ToListAsync();
+
+            return profiles;
+        }
+
+        public async Task<BugChaseProfile> CreateProfile(int userId)
+        {
+            var profile = new BugChaseProfile()
+            {
+                UserId = userId,
+                MaxScore = 0
+            };
+            await _dbContext.BugChaseProfiles.AddAsync(profile);
+            await _dbContext.SaveChangesAsync();
+
+            return profile;
+        }
+
+        public async Task UpdateProfile(BugChaseProfile bugChaseProfile)
+        {
+            var profile = await _dbContext.BugChaseProfiles.FirstAsync(p => p.UserId == bugChaseProfile.UserId);
+            profile.MaxScore = bugChaseProfile.MaxScore;
+
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
