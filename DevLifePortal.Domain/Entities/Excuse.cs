72b11d9d2c8c57c2d0d59857b1a7b823514c43e5
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 11:00:58 +0400

    create excuse entity

---
+++ DevLifePortal.Domain/Entities/Excuse.cs
@@ -0,0 +1,10 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class Excuse
+    {
+        public Guid Id { get; set; } 
+        public string Category { get; set; }
+        public string Type { get; set; }    
+        public string Text { get; set; }
+    }
+}
