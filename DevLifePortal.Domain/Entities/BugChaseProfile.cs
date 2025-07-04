39a13abbe182773b121e597691eef8c653fba921
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 09:54:39 +0400

    create bug chase proflie entity

---
+++ DevLifePortal.Domain/Entities/BugChaseProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class BugChaseProfile
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MaxScore { get; set; }
+
+        public User User { get; set; }
+    }
+}
