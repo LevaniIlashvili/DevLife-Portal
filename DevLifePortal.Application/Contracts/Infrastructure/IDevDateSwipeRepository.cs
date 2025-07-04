41d406c1f031f8c400f29bc2e2984024f1fc171e
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:16:19 +0400

    implement dev date swipe repository

---
+++ DevLifePortal.Application/Contracts/Infrastructure/IDevDateSwipeRepository.cs
@@ -0,0 +1,11 @@
+﻿using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Contracts.Infrastructure
+{
+    public interface IDevDateSwipeRepository
+    {
+        Task<List<Guid>> GetSwipedFakeProfileIdsAsync(int userId);
+        Task SaveSwipeAsync(DevDatingSwipeAction swipe);
+        Task<List<Guid>> GetRightSwipedFakeProfileIdsAsync(int userId);
+    }
+}
---
+++ DevLifePortal.Infrastructure/Repositories/DevDateSwipeRepository.cs
@@ -0,0 +1,54 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace DevLifePortal.Infrastructure.Repositories
+{
+    public class DevDateSwipeRepository : IDevDateSwipeRepository
+    {
+        private readonly IMongoCollection<DevDatingSwipeAction> _swipeCollection;
+
+        public DevDateSwipeRepository(IMongoClient mongoClient, IConfiguration configuration)
+        {
+            var dbName = configuration.GetConnectionString("MongoDb");
+            var db = mongoClient.GetDatabase("DevLife");
+            _swipeCollection = db.GetCollection<DevDatingSwipeAction>("swipe_actions");
+        }
+
+        public async Task SaveSwipeAsync(DevDatingSwipeAction swipe)
+        {
+            await _swipeCollection.InsertOneAsync(swipe);
+        }
+
+        public async Task<List<Guid>> GetSwipedFakeProfileIdsAsync(int userId)
+        {
+            var filter = Builders<DevDatingSwipeAction>.Filter.Eq(s => s.UserId, userId);
+            var projection = Builders<DevDatingSwipeAction>.Projection.Include(s => s.TargetProfileId);
+
+            var swipes = await _swipeCollection
+                .Find(filter)
+                .Project<DevDatingSwipeAction>(projection)
+                .ToListAsync();
+
+            return swipes.Select(s => s.TargetProfileId).ToList();
+        }
+
+        public async Task<List<Guid>> GetRightSwipedFakeProfileIdsAsync(int userId)
+        {
+            var filter = Builders<DevDatingSwipeAction>.Filter.And(
+                Builders<DevDatingSwipeAction>.Filter.Eq(s => s.UserId, userId),
+                Builders<DevDatingSwipeAction>.Filter.Eq(s => s.Direction, "right")
+            );
+
+            var projection = Builders<DevDatingSwipeAction>.Projection.Include(s => s.TargetProfileId);
+
+            var swipes = await _swipeCollection
+                .Find(filter)
+                .Project<DevDatingSwipeAction>(projection)
+                .ToListAsync();
+
+            return swipes.Select(s => s.TargetProfileId).ToList();
+        }
+    }
+}
