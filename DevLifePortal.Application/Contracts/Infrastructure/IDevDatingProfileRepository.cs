3c21ee426929397e80a2df9952340a3beb53759c
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:13:03 +0400

    implement dev dating profile repository

---
+++ DevLifePortal.Application/Contracts/Infrastructure/IDevDatingProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Contracts.Infrastructure
+{
+    public interface IDevDatingProfileRepository
+    {
+        Task<DevDatingProfile?> GetAsync(int userId);
+        Task<DevDatingProfile> AddAsync(DevDatingProfile devDatingProfile);
+    }
+}
---
+++ DevLifePortal.Infrastructure/Repositories/DevDatingProfileRepository.cs
@@ -0,0 +1,29 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevLifePortal.Infrastructure.Repositories
+{
+    public class DevDatingProfileRepository : IDevDatingProfileRepository
+    {
+        private readonly DevLifeDbContext _dbContext;
+
+        public DevDatingProfileRepository(DevLifeDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<DevDatingProfile?> GetAsync(int userId)
+        {
+            var profile = await _dbContext.DevDatingProfiles.FirstOrDefaultAsync(p => p.UserId ==  userId);
+            return profile;
+        }
+
+        public async Task<DevDatingProfile> AddAsync(DevDatingProfile profile)
+        {
+            await _dbContext.AddAsync(profile);
+            await _dbContext.SaveChangesAsync();
+            return profile;
+        }
+    }
+}
