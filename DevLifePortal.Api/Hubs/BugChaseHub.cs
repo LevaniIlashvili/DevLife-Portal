95c5c393fcbe1d3274528b985c722ac315f782ac
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 10:01:12 +0400

    create bug chase hub for realtime leaderboard

---
+++ DevLifePortal.Api/Hubs/BugChaseHub.cs
@@ -0,0 +1,35 @@
+﻿using DevLifePortal.Application.Contracts.Application;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DevLifePortal.Api.Hubs
+{
+    public class BugChaseHub : Hub
+    {
+        private readonly IBugChaseService _bugChaseService;
+
+        public BugChaseHub(IBugChaseService bugChaseService)
+        {
+            _bugChaseService = bugChaseService;
+        }
+
+        public async Task SendScore(int score)
+        {
+            var httpContext = Context.GetHttpContext();
+
+            var userId = httpContext.Session.GetString("UserId");
+
+            await _bugChaseService.UpdateProfileScore(int.Parse(userId), score);
+
+            var topProfiles = await _bugChaseService.GetTopProfiles();
+            await Clients.All.SendAsync("LeaderboardUpdated", topProfiles);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            var leaderboard = await _bugChaseService.GetTopProfiles();
+            await Clients.Caller.SendAsync("LeaderboardUpdated", leaderboard);
+
+            await base.OnConnectedAsync();
+        }
+    }
+}
