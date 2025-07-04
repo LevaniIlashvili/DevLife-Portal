b804d6a3510092f19faa4fe502a9d7b807532c31
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 11:01:57 +0400

    implement excuse generator services

---
+++ DevLifePortal.Api/Endpoints/ExcuseGeneratorEndpoints.cs
@@ -0,0 +1,45 @@
+﻿using DevLifePortal.Application.Contracts.Application;
+using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Api.Endpoints
+{
+    public static class ExcuseEndpoints
+    {
+        public static void MapExcuseEndpoints(this IEndpointRouteBuilder app)
+        {
+            var excusesGroup = app.MapGroup("/excuses");
+
+            excusesGroup.MapGet("/generate", (IExcuseGeneratorService excuseGeneratorService, string category, string type) =>
+            {
+                var excuse = excuseGeneratorService.Generate(category, type);
+                return Results.Ok(excuse);
+            });
+
+            excusesGroup.MapPost("/favorite", async (
+                Excuse excuse,
+                IExcuseGeneratorService excuseGeneratorService,
+                HttpContext context) =>
+            {
+                var userId = context.Session.GetString("UserId");
+
+                await excuseGeneratorService.SaveFavoriteAsync(userId, excuse);
+                return Results.Ok();
+            });
+
+            excusesGroup.MapGet("/favorites", async (
+                IExcuseGeneratorService excuseGeneratorService,
+                HttpContext context) =>
+            {
+                var userId = context.Session.GetString("UserId");
+
+                if (userId == null)
+                {
+                    return Results.Unauthorized();
+                }
+
+                var favorites = await excuseGeneratorService.GetFavoritesAsync(userId);
+                return Results.Ok(favorites);
+            });
+        }
+    }
+}
