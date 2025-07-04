d50c88c4c79a60140248f1ed8fda8cd1513bfedd
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:11:40 +0400

    create user profile for devdating

---
+++ DevLifePortal.Domain/Entities/DevDatingProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class DevDatingProfile
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public bool IsMale { get; set; }
+        public bool PrefersMale { get; set; }
+        public string Bio {  get; set; }
+
+        public User User { get; set; }
+    }
+}
