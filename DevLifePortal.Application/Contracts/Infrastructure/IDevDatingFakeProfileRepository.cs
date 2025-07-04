ebdcf8bbd720b8d84da894d69c7f3ba1bfe5ebca
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:16:01 +0400

    implement dev dating fake profile repository

---
+++ DevLifePortal.Application/Contracts/Infrastructure/IDevDatingFakeProfileRepository.cs
@@ -0,0 +1,10 @@
+﻿using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Contracts.Infrastructure
+{
+    public interface IDevDatingFakeProfileRepository
+    {
+        Task<List<DevDatingFakeProfile>> GetAllAsync();
+        Task<DevDatingFakeProfile?> GetByIdAsync(Guid id);
+    }
+}
---
+++ DevLifePortal.Infrastructure/Repositories/DevDatingFakeProfileRepository.cs
@@ -0,0 +1,30 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace DevLifePortal.Infrastructure.Repositories
+{
+    public class DevDatingFakeProfileRepository : IDevDatingFakeProfileRepository
+    {
+        private readonly IMongoCollection<DevDatingFakeProfile> _profileCollection;
+
+        public DevDatingFakeProfileRepository(IMongoClient mongoClient, IConfiguration configuration)
+        {
+            var dbName = configuration.GetConnectionString("MongoDb");
+            var db = mongoClient.GetDatabase("DevLife");
+            _profileCollection = db.GetCollection<DevDatingFakeProfile>("fake_profiles");
+        }
+
+        public async Task<List<DevDatingFakeProfile>> GetAllAsync()
+        {
+            return await _profileCollection.Find(_ => true).ToListAsync();
+        }
+
+        public async Task<DevDatingFakeProfile?> GetByIdAsync(Guid id)
+        {
+            var filter = Builders<DevDatingFakeProfile>.Filter.Eq(p => p.Id, id);
+            return await _profileCollection.Find(filter).FirstOrDefaultAsync();
+        }
+    }
+}
