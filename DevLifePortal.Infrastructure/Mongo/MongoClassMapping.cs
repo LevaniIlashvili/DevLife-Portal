d0e7ee11dac249c194c7bf46e5a1c97a3740cb6b
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:15:37 +0400

    map entities

---
+++ DevLifePortal.Infrastructure/Mongo/MongoClassMapping.cs
@@ -0,0 +1,38 @@
+﻿using DevLifePortal.Domain.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Serializers;
+
+namespace DevLifePortal.Infrastructure.Mongo
+{
+    public static class MongoClassMapping
+    {
+        public static void RegisterMappings()
+        {
+            if (!BsonClassMap.IsClassMapRegistered(typeof(DevDatingFakeProfile)))
+            {
+                BsonClassMap.RegisterClassMap<DevDatingFakeProfile>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.MapIdProperty(c => c.Id)
+                      .SetSerializer(new GuidSerializer(BsonType.String));
+                });
+            }
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(DevDatingSwipeAction)))
+            {
+                BsonClassMap.RegisterClassMap<DevDatingSwipeAction>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.MapIdMember(c => c.Id)
+                      .SetIdGenerator(GuidGenerator.Instance)
+                      .SetSerializer(new GuidSerializer(BsonType.String));
+                    cm.MapMember(c => c.TargetProfileId)
+                      .SetSerializer(new GuidSerializer(BsonType.String));
+                });
+            }
+
+        }
+    }
+}
