7cbf167d3e953e44250ecbe0aa36a42dbf2cae43
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:12:03 +0400

    add migration for devdate user profile

---
+++ DevLifePortal.Infrastructure/Migrations/20250704101900_AddDevDatingProfileDefineRelationshipWithUser.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DevLifePortal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDevDatingProfileDefineRelationshipWithUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DevDatingProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    IsMale = table.Column<bool>(type: "boolean", nullable: false),
+                    PrefersMale = table.Column<bool>(type: "boolean", nullable: false),
+                    Bio = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevDatingProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DevDatingProfiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DevDatingProfiles_UserId",
+                table: "DevDatingProfiles",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DevDatingProfiles");
+        }
+    }
+}
