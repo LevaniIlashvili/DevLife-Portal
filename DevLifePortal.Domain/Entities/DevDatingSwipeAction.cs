f084e627b81469ab9733353607f7f1ec003be546
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 18:13:42 +0400

    create dev dating swipe action entity

---
+++ DevLifePortal.Domain/Entities/DevDatingSwipeAction.cs
@@ -0,0 +1,10 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class DevDatingSwipeAction
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public int UserId { get; set; }
+        public Guid TargetProfileId { get; set; }
+        public string Direction { get; set; } = string.Empty;
+    }
+}
