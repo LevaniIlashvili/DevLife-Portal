55dc3a87a2eb06ac929f669d86cf7ce3a106be32
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-04 09:59:14 +0400

    create user dto

---
+++ DevLifePortal.Application/DTOs/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; }
+        public string TechStack { get; set; } = string.Empty;
+        public string ExperienceLevel { get; set; } = string.Empty;
+        public string ZodiacSign { get; set; } = string.Empty;
+    }
+}
