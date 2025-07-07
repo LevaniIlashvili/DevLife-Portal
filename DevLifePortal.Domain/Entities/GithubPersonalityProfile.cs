f5f606dcfb9bd03210bb4376a7103e12998535b1
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-07 16:03:47 +0400

    create entities for github repo analyzer

---
+++ DevLifePortal.Domain/Entities/GithubPersonalityProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class GithubPersonalityProfile
+    {
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public List<string> Strengths { get; set; }
+        public List<string> Weaknesses { get; set; }
+        public string CelebrityMatch { get; set; }
+    }
+}
---
+++ DevLifePortal.Domain/Entities/GithubRepoAnalysisResult.cs
@@ -0,0 +1,10 @@
+﻿namespace DevLifePortal.Domain.Entities
+{
+    public class GithubRepoAnalysisResult
+    {
+        public string RepoName { get; set; }
+        public List<string> TopLevelFiles { get; set; }
+        public List<string> RecentCommitMessages { get; set; }
+        public GithubPersonalityProfile Personality { get; set; }
+    }
+}
