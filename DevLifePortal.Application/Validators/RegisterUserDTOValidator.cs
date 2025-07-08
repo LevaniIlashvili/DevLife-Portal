af27a2c1eddc8caea18376e411dcdd0e60ff56e6
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 11:45:44 +0400

    add register user dto validator

---
+++ DevLifePortal.Application/Validators/RegisterUserDTOValidator.cs
@@ -0,0 +1,32 @@
+﻿using DevLifePortal.Application.DTOs;
+using FluentValidation;
+
+namespace DevLifePortal.Application.Validators
+{
+    public class UserDtoValidator : AbstractValidator<RegisterUserDTO>
+    {
+        public UserDtoValidator()
+        {
+            RuleFor(u => u.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .MinimumLength(3).WithMessage("Username must be at least 3 characters.");
+
+            RuleFor(u => u.FirstName)
+                .NotEmpty().WithMessage("First name is required.");
+
+            RuleFor(u => u.LastName)
+                .NotEmpty().WithMessage("Last name is required.");
+
+            RuleFor(u => u.DateOfBirth)
+                .LessThan(DateOnly.FromDateTime(DateTime.Now))
+                .WithMessage("Date of birth must be in the past.");
+
+            RuleFor(u => u.TechStack)
+                .NotEmpty().WithMessage("Tech stack is required.");
+
+            RuleFor(u => u.ExperienceLevel)
+                .NotEmpty().WithMessage("Experience level is required.");
+        }
+    }
+
+}
