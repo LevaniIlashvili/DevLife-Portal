4ac92ebb1f6ac72ee681ff934eb75ba03ca25cdd
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 13:09:45 +0400

    remove id property

--- DevLifePortal.Application/DTOs/RegisterUserDTO.cs
+++ DevLifePortal.Application/DTOs/RegisterUserDTO.cs
@@ -2,7 +2,6 @@
 {
     public class RegisterUserDTO
     {
-        public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
