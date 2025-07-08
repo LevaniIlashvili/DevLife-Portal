6d3b5502c8e0052c89418caa9eff99f9276a7b5b
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 16:07:52 +0400

    implement code casino challenge repository

---
+++ DevLifePortal.Infrastructure/Repositories/CodeCasinoChallengeRepository.cs
@@ -0,0 +1,32 @@
+﻿using DevLifePortal.Application.Contracts.Infrastructure;
+using DevLifePortal.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace DevLifePortal.Infrastructure.Repositories
+{
+    public class CodeCasinoChallengeRepository : ICodeCasinoChallengeRepository
+    {
+        private readonly IMongoCollection<CodeCasinoChallenge> _challengeCollection;
+
+        public CodeCasinoChallengeRepository(IMongoClient mongoClient, IConfiguration configuration)
+        {
+            var dbName = configuration.GetConnectionString("MongoDb");
+            var db = mongoClient.GetDatabase("DevLife");
+            _challengeCollection = db.GetCollection<CodeCasinoChallenge>("code_casino_challenges");
+        }
+
+        public async Task<List<CodeCasinoChallenge>> GetAllAsync()
+        {
+            return await _challengeCollection
+                .Find(_ => true)
+                .Project(x => new CodeCasinoChallenge
+                {
+                    CorrectCode = x.CorrectCode,
+                    IncorrectCode = x.IncorrectCode,
+                    TechStack = x.TechStack
+                })
+                .ToListAsync();
+        }
+    }
+}
