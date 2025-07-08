653a87ff47a147c5a17873b333ef826f07fec00d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 13:41:13 +0400

    create dev dating swipe action dto

---
+++ DevLifePortal.Application/DTOs/DevDatingSwipeActionDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class DevDatingSwipeActionDTO
+    {
+        public Guid TargetProfileId { get; set; }
+        public string Direction { get; set; } = string.Empty;
+    }
+}
