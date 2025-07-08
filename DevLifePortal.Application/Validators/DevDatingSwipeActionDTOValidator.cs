c2a140d73237dd9d542975732c1ddad3a21a0840
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 13:42:29 +0400

    create validator for swipe action dto

---
+++ DevLifePortal.Application/Validators/DevDatingSwipeActionDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using DevLifePortal.Application.DTOs;
+using FluentValidation;
+
+namespace DevLifePortal.Application.Validators
+{
+    public class DevDatingSwipeActionDTOValidator : AbstractValidator<DevDatingSwipeActionDTO>
+    {
+        public DevDatingSwipeActionDTOValidator()
+        {
+            RuleFor(x => x.TargetProfileId)
+                .NotEmpty()
+                .WithMessage("TargetProfileId must not be empty");
+
+            RuleFor(x => x.Direction)
+                .NotEmpty()
+                .WithMessage("Swipe direction is required")
+                .Must(d => d.Equals("left", StringComparison.OrdinalIgnoreCase) || d.Equals("right", StringComparison.OrdinalIgnoreCase))
+                .WithMessage("Direction must be either 'left' or 'right'");
+        }
+    }
+}
