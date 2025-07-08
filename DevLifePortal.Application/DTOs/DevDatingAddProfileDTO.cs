15aa3ccce6eab7593378c137f040579840d0cc07
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 11:47:11 +0400

    add dtos for dev dating

---
+++ DevLifePortal.Application/DTOs/DevDatingAddProfileDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class DevDatingAddProfileDTO
+    {
+        public bool IsMale { get; set; }
+        public bool PrefersMale { get; set; }
+        public string Bio { get; set; }
+    }
+}
---
+++ DevLifePortal.Application/DTOs/DevDatingProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class DevDatingProfileDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public bool IsMale { get; set; }
+        public bool PrefersMale { get; set; }
+        public string Bio { get; set; }
+    }
+}
