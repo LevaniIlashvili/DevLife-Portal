59bc2e719dac9c24de8cf5cfd3a7247412437feb
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 13:10:51 +0400

    create validator for code roast solution dto

---
+++ DevLifePortal.Application/Validators/CodeRoastSolutionDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using DevLifePortal.Application.DTOs;
+using FluentValidation;
+
+namespace DevLifePortal.Application.Validators
+{
+    public class CodeRoastSolutionDTOValidator : AbstractValidator<CodeRoastSolutionDTO>
+    {
+        public CodeRoastSolutionDTOValidator()
+        {
+            RuleFor(u => u.ProblemName)
+                .NotEmpty().WithMessage("Problem name is required.");
+
+            RuleFor(u => u.Solution)
+                .NotEmpty().WithMessage("Solution is required.");
+        }
+    }
+}
