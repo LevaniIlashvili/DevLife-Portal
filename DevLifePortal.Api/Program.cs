f7ceb74ef7cdd2dbbb93b83b6d398416e8828649
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 11:24:00 +0400

    add swagger ui

--- DevLifePortal.Api/Program.cs
+++ DevLifePortal.Api/Program.cs
@@ -10,8 +10,8 @@ using MongoDB.Driver;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSignalR();
 
@@ -50,7 +50,12 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "DevLifePortal API v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseCustomExceptionHandler();
