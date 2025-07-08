13d3d994788ccfd6e1763e3e5f9c887d52c0a9a6
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 11:44:25 +0400

    add mapping profile

---
+++ DevLifePortal.Application/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DevLifePortal.Application.DTOs;
+using DevLifePortal.Domain.Entities;
+
+namespace DevLifePortal.Application.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterUserDTO, User>().ReverseMap();
+            CreateMap<UserDTO, User>().ReverseMap();
+
+            CreateMap<DevDatingProfile, DevDatingAddProfileDTO>().ReverseMap();
+            CreateMap<DevDatingProfileDTO, DevDatingProfile>().ReverseMap();
+        }
+    }
+}
