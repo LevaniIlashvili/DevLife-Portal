ae2ea4077ff86834ce4e0b23f1735557b45f4ea2
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-08 12:20:52 +0400

    create dev dating text match dto

---
+++ DevLifePortal.Application/DTOs/DevDatingTextMatchDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DevLifePortal.Application.DTOs
+{
+    public class DevDatingTextMatchDTO
+    {
+        public Guid FakeProfileId { get; set; }
+        public string UserText { get; set; }
+    }
+}
