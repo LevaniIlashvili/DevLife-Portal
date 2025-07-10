a212a81d1e2dfea66de414c2434c11de5bcfe3e7
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-10 11:50:37 +0400

    feat: validate tech stack in user registration

--- DevLifePortal.Application/Validators/RegisterUserDTOValidator.cs
+++ DevLifePortal.Application/Validators/RegisterUserDTOValidator.cs
@@ -1,4 +1,5 @@
 ﻿using DevLifePortal.Application.DTOs;
+using DevLifePortal.Domain.Enums;
 using FluentValidation;
 
 namespace DevLifePortal.Application.Validators
@@ -8,21 +9,22 @@ namespace DevLifePortal.Application.Validators
         public RegisterUserDTOValidator()
         {
             RuleFor(u => u.Username)
-                .NotEmpty().WithMessage("Username is required.")
-                .MinimumLength(3).WithMessage("Username must be at least 3 characters.");
+                .NotEmpty().WithMessage("Username is required")
+                .MinimumLength(3).WithMessage("Username must be at least 3 characters");
 
             RuleFor(u => u.FirstName)
-                .NotEmpty().WithMessage("First name is required.");
+                .NotEmpty().WithMessage("First name is required");
 
             RuleFor(u => u.LastName)
-                .NotEmpty().WithMessage("Last name is required.");
+                .NotEmpty().WithMessage("Last name is required");
 
             RuleFor(u => u.DateOfBirth)
                 .LessThan(DateOnly.FromDateTime(DateTime.Now))
-                .WithMessage("Date of birth must be in the past.");
+                .WithMessage("Date of birth must be in the past");
 
             RuleFor(u => u.TechStack)
-                .NotEmpty().WithMessage("Tech stack is required.");
+                .Must(value => Enum.TryParse<TechStack>(value, true, out _))
+                .WithMessage("Tech stack must be a valid value");
 
             RuleFor(u => u.ExperienceLevel)
                 .NotEmpty().WithMessage("Experience level is required.");
