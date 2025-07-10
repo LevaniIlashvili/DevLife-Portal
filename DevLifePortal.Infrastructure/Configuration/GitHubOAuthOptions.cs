a255ff6a5361b94ee42de8b8ce15bc065bbe444d
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-10 18:12:35 +0400

    refactor: add github options class

---
+++ DevLifePortal.Infrastructure/Configuration/GitHubOAuthOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace DevLifePortal.Infrastructure.Configuration
+{
+    public class GitHubOAuthOptions
+    {
+        public string ClientId { get; set; } = "";
+        public string ClientSecret { get; set; } = "";
+        public string CallbackUrl { get; set; } = "";
+    }
+}
