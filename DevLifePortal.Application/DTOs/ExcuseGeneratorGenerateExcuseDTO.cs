15b44a830deb7fda58a10960452641bf17234b6e
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-11 12:25:59 +0400

    feat: add ExcuseGeneratorGenerateExcuseDTO

---
+++ DevLifePortal.Application/DTOs/ExcuseGeneratorGenerateExcuseDTO.cs
@@ -0,0 +1,10 @@
+﻿using DevLifePortal.Domain.Enums;
+
+namespace DevLifePortal.Application.DTOs
+{
+    public class ExcuseGeneratorGenerateExcuseDTO
+    {
+        public string Category { get; set; }
+        public ExcuseType Type { get; set; }
+    }
+}
