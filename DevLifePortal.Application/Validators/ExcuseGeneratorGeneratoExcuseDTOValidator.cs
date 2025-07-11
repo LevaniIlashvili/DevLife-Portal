3811b1f845f6c776c302574ef53270b8bbc4030a
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-11 12:26:14 +0400

    feat: add validator for ExcuseGeneratorGeneratoExcuseDTO

---
+++ DevLifePortal.Application/Validators/ExcuseGeneratorGeneratoExcuseDTOValidator.cs
@@ -0,0 +1,15 @@
+﻿using DevLifePortal.Application.DTOs;
+using FluentValidation;
+
+namespace DevLifePortal.Application.Validators
+{
+    public class ExcuseGeneratorGeneratoExcuseDTOValidator : AbstractValidator<ExcuseGeneratorGenerateExcuseDTO>
+    {
+        public ExcuseGeneratorGeneratoExcuseDTOValidator()
+        {
+            RuleFor(e => e.Category).NotEmpty();
+
+            RuleFor(e => e.Type).IsInEnum();
+        }
+    }
+}
