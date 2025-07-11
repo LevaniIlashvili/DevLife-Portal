0d1727eeb8f653535aca7be92adfc7c2f581f191
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-07-11 16:27:44 +0400

    feat: apply configurations from assembly

---
+++ DevLifePortal.Infrastructure/Postgres/DevLifeDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DevLifePortal.Domain.Entities;
+using DevLifePortal.Infrastructure.Postgres.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevLifePortal.Infrastructure.Postgres
+{
+    public class DevLifeDbContext : DbContext  
+    {
+        public DevLifeDbContext(DbContextOptions<DevLifeDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<CodeCasinoProfile> CodeCasinoProfiles => Set<CodeCasinoProfile>();
+        public DbSet<BugChaseProfile> BugChaseProfiles => Set<BugChaseProfile>();
+        public DbSet<DevDatingProfile> DevDatingProfiles => Set<DevDatingProfile>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UserConfigurations).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
